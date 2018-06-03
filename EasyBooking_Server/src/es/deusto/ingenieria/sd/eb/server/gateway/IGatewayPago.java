47e7815b050ee27f08231dad03db15885191128f
Benny96 <benatgalaizp@gmail.com>
2018-06-03 15:26:00 +0200

    Ordenando todo, parte III

--- EasyBooking_Server/src/es/deusto/ingenieria/sd/eb/server/gateway/IGatewayPago.java
+++ EasyBooking_Server/src/es/deusto/ingenieria/sd/eb/server/gateway/IGatewayPago.java
@@ -1,5 +1,9 @@
 package es.deusto.ingenieria.sd.eb.server.gateway;
 
+/**
+ * Variación de Strategy hecha con Interfaz para Pago en vez de clase abstracta.
+ * @author Beñat
+ */
 public interface IGatewayPago {
 	public int efectuarPago(String correo, double cantidad);
 }
